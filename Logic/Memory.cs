a2644b679a9fa3260acef6141e6845aad866e19f
Steven Casper <sebastiancasper3@gmail.com>
2020-11-10 05:05:01 -0500

    Remove unused imports

--- Logic/Memory.cs
+++ Logic/Memory.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using LiveSplit.BattleForBikiniBottom.UI;
 using LiveSplit.ComponentUtil;
 
 namespace LiveSplit.BattleForBikiniBottom.Logic
--- UI/Component.cs
+++ UI/Component.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.Windows.Forms;
 using System.Xml;
 using LiveSplit.BattleForBikiniBottom.Logic;
